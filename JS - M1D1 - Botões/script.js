1d3a7f9d6b82b9d4ca7f60ab9c290379b6dcc45e
Gabriel Soares Alves Bernardo da Silva <100007367+gabrielsoarespebr@users.noreply.github.com>
2022-08-15 14:28:08 -0300

    JS - M1D1
    Projeto associando a tag button do HTML à função alert do JavaScript.

---
+++ JS - M1D1 - Botões/script.js
@@ -0,0 +1,11 @@
+function botao1(){
+    alert("Você clicou no botão 1.");
+}
+
+function botao2(){
+    alert("Você clicou no botão 2.");
+}
+
+function botao3(){
+    alert("Você clicou no botão 3.");
+}
